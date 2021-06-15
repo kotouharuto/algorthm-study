5791d290285d5f33c7ee0c8129c4ff0725ec9938
kotouharuto <haruto.k.0321@gmail.com>
2021-06-16 05:11:10 +0900

    繰り返し過程における条件判定。(記号文字+と-を交互に表示させる) ファイル2はプログラムの柔軟性を上げたもの。

---
+++ basic-algorithem_1/1-2/alteranative1.py
@@ -0,0 +1,12 @@
+# 記号文字+と-を交互に表示(その1)
+
+print('記号文字+と-を交互に表示します。')
+n = int(input('全部で何個：'))
+
+for i in range(n):
+ if i % 2: # 奇数
+  print('-', end = '')
+ else: # 偶数
+  print('+', end = '')
+
+print()
---
+++ basic-algorithem_1/1-2/alteranative2.py
@@ -0,0 +1,12 @@
+# 記号文字+と-を交互に表示(その2)
+
+print('記号文字+と-を交互に表示します。')
+n = int(input('全部で何個：'))
+
+for _ in range(n // 2):
+ print('+-', end = '')
+
+if n % 2:
+ print('+', end = '')
+
+print()
